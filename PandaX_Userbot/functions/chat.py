b7049b085e0fd90de28b7058987ab960a4e967b7
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-02 09:48:20 +0700

    Create chat.py

---
+++ PandaX_Userbot/functions/chat.py
@@ -0,0 +1,39 @@
+from .. import udB
+
+try:
+    eval(udB["CHATBOT_USERS"])
+except BaseException:
+    udB.set("CHATBOT_USERS", "{}")
+
+
+def get_all_added(chat):
+    ok = eval(udB["CHATBOT_USERS"])
+    if ok.get(chat):
+        return ok[chat]
+    return False
+
+
+def chatbot_stats(chat, id):
+    ok = eval(udB["CHATBOT_USERS"])
+    if ok.get(chat):
+        if id in ok[chat]:
+            return True
+    return False
+
+
+def add_chatbot(chat, id):
+    ok = eval(udB["CHATBOT_USERS"])
+    if not ok.get(chat):
+        ok.update({chat: [id]})
+    else:
+        if id not in ok[chat]:
+            ok[chat].append(id)
+    return udB.set("CHATBOT_USERS", str(ok))
+
+
+def rem_chatbot(chat, id):
+    ok = eval(udB["CHATBOT_USERS"])
+    if ok.get(chat):
+        if id in ok[chat]:
+            ok[chat].remove(id)
+    return udB.set("CHATBOT_USERS", str(ok))
