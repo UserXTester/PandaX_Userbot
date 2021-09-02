4c71514f6d126c16f2e67dae7480d9543d0e65d2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-03 00:00:33 +0700

    Update _InlineBot.py

--- PandaX_v20/_InlineBot.py
+++ PandaX_v20/_InlineBot.py
@@ -464,7 +464,7 @@ async def _(event):
     cpuUsage = psutil.cpu_percent()
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    pin = f"➣ ☬ Pengguna 𝐏𝐚𝐧𝐝𝐚𝐗_𝐔𝐬𝐞𝐫𝐛𝐨𝐭 ☬\n\nNama - {owner}\n➣ ☬ Plugins - {Plugins}\n➣ ☬ Modules - {Modules}\nTotal Command :{command}\nCPU: {cpuUsage}\nRAM : {memory}\nDISK : {disk}"
+    pin = f"➣ ☬ Pengguna 𝐏𝐚𝐧𝐝𝐚𝐗_𝐔𝐬𝐞𝐫𝐛𝐨𝐭 ☬\n\nNama - {owner}\n➣ ☬ Plugins - {Plugins}\n➣ ☬ Modules - {Modules}\nTotal Command : {command}\nCPU: {cpuUsage}\nRAM : {memory}\nDISK : {disk}"
     await event.answer(pin, cache_time=0, alert=True)
 
     
