b74123796ed9c8ffbddb7d4771b945a8467815e5
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-02 23:59:36 +0700

    Update _InlineBot.py

--- PandaX_v20/_InlineBot.py
+++ PandaX_v20/_InlineBot.py
@@ -460,10 +460,11 @@ async def _(event):
     owner = OWNER_NAME
     Plugins = len(PLUGINS)
     Modules = len(MODULES)
+    command = len(z)
     cpuUsage = psutil.cpu_percent()
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
-    pin = f"➣ ☬ Pengguna 𝐏𝐚𝐧𝐝𝐚𝐗_𝐔𝐬𝐞𝐫𝐛𝐨𝐭 ☬\n\nNama - {owner}\n➣ ☬ Plugins - {Plugins}\n➣ ☬ Modules - {Modules}\nCPU: {cpuUsage}\nRAM : {memory}\nDISK : {disk}"
+    pin = f"➣ ☬ Pengguna 𝐏𝐚𝐧𝐝𝐚𝐗_𝐔𝐬𝐞𝐫𝐛𝐨𝐭 ☬\n\nNama - {owner}\n➣ ☬ Plugins - {Plugins}\n➣ ☬ Modules - {Modules}\nTotal Command :{command}\nCPU: {cpuUsage}\nRAM : {memory}\nDISK : {disk}"
     await event.answer(pin, cache_time=0, alert=True)
 
     
