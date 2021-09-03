893a2b76cb91f4dc1e2e9818e55a081da3177a0a
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-03 09:53:33 +0700

    Update DataPanda.py

--- PandaX_v20/DataPanda.py
+++ PandaX_v20/DataPanda.py
@@ -4,7 +4,7 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
+# Panda Editor by ilham mansiz
 
 
 """
