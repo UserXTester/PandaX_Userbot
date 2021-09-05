c295c8aca804fdc1901d8f82cbfa724c20dab8d0
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-05 14:02:52 +0700

    Update __main__.py

--- PandaX_Userbot/__main__.py
+++ PandaX_Userbot/__main__.py
@@ -395,17 +395,9 @@ if Plug_channel:
     petercordpanda_bot.loop.run_until_complete(plug())
 petercordpanda_bot.loop.run_until_complete(ready())
 
-PLUGINS = dict(
-    root="PandaX_Userbot",
-    include=[
-        "vc." + Var.VC_PLUGIN,
-        "ping",
-        "sysinfo"
-    ]
-)
+
 
 if __name__ == "__main__":
-    app = plugins=PLUGINS
     app.start()
     print(suc_msg)
     idle()
