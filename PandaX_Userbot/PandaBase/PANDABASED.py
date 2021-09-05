70c72f92587f73e3a40588b97ef64e298619cb0f
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-05 22:08:08 +0700

    Create PANDABASED.py

---
+++ PandaX_Userbot/PandaBase/PANDABASED.py
@@ -0,0 +1,5 @@
+from PandaX_Userbot.PandaBase.antiflood_sql import *
+from PandaX_Userbot.PandaBase.blacklist_sql import *
+from PandaX_Userbot.PandaBase.filter_sql import *
+from PandaX_Userbot.PandaBase.gban_sql import *
+from PandaX_Userbot.PandaBase.gmute_sql import *
