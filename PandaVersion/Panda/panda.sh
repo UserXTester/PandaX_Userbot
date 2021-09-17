6e29462b550e79d26cfe13eb006a188c9361be83
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-09-17 08:59:49 +0700

    Create panda.sh

---
+++ PandaVersion/Panda/panda.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
+echo "
+            PandaUserbot
+"
+
+python3 -m PandaX_Userbot
