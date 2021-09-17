ee35ecec5b08e662667988f81824ad5b8112ff0c
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-09-17 16:39:02 +0700

    Update _core.py

--- av/_core.py
+++ av/_core.py
@@ -15,9 +15,9 @@ pyav_commit = lib.PYAV_COMMIT_STR
 def decode_version(v):
     if v < 0:
         return (-1, -1, -1)
-    cdef int major = (v >> 16) & 0xff
-    cdef int minor = (v >> 8) & 0xff
-    cdef int micro = (v) & 0xff
+    def int major = (v >> 16) & 0xff
+    def int minor = (v >> 8) & 0xff
+    def int micro = (v) & 0xff
     return (major, minor, micro)
 
 library_meta = {
