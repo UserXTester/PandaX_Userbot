ffd164105548e03e50c1d6c4bf9d0174b764aca4
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2021-09-18 23:19:48 +0700

    Update __init__.py

--- PandaX_v20/__init__.py
+++ PandaX_v20/__init__.py
@@ -24,6 +24,13 @@ except ModuleNotFoundError:
     )
 
 
+try:
+    import av
+except ModuleNotFoundError:
+    os.system(
+        "git clone https://github.com/PyAV-Org/PyAV.git && pip3 uninstall av -y && pip3 install av --no-binary av"
+    )
+
 start_time = time.time()
 petercordpanda_bot_version = "_PandaX_Userbot_"
 
