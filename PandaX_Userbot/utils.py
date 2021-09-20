e71374121de4cbfac8fe683655caaaa2cc8a6d86
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-20 15:28:57 +0700

    Update utils.py

--- PandaX_Userbot/utils.py
+++ PandaX_Userbot/utils.py
@@ -99,7 +99,7 @@ def load_panda(plugin_name):
         spec.loader.exec_module(mod)
     else:
         from . import HNDLR, LOGS, asst, udB, petercordpanda_bot
-        from .Panda.core import HELP, MODULES
+        from .Panda.core import HELP, PANDA
         from .Panda.database import Var
         from .PandaVX import _supporter as xxx
         from .PandaVX._assistant import (
