5e3c6d30cea6ed624b20e15eed032a0e5325ceb6
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-23 12:52:34 +0700

    Update DaftarModules.py

--- PandaX_v20/DaftarModules.py
+++ PandaX_v20/DaftarModules.py
@@ -4,55 +4,36 @@ from platform import uname
 
 from PandaX_Userbot.Panda.core import *
 
+import asyncio
+from platform import uname
+
+modules = CMD_HELP
+
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
 
 
-
-
-@ilhammansiz_cmd(pattern="daftarplugin ?(.*)")
-async def help(event):
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(rambot):
     """ For .help command,"""
-    args = event.pattern_match.group(1).lower()
-    if args:
-        if args in HELP:
-            await event.edit(str(HELP[args]))
-        else:
-            await event.edit("**`Command Tidak Ditemukan, Harap Ketik Command Dengan Benar`**")
-            await asyncio.sleep(200)
-            await event.delete()
-    else:
-        string = ""
-        for i in HELP:
-            string += "`" + str(i)
-            string += "`\t 🖨  "
-        await event.edit("**PandaX_Userbot**\n\n"
-                         f"**◉ Bᴏᴛ ᴏꜰ PandaX_Userbot Plugin**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(HELP)}**\n\n"
-                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n")
-        await event.reply(f"\n**Contoh** : Ketik <`.help afk`> Untuk Informasi Pengunaan.\nAtau Bisa Juga Ketik `.helpme` Untuk Main Menu Yang Lain-Nya.")
-        await asyncio.sleep(1000)
-        await event.delete()
-
-
-@ilhammansiz_cmd(pattern="daftarmodules ?(.*)")
-async def help(event):
-    """ For .help command,"""
-    args = event.pattern_match.group(1).lower()
+    args = rambot.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
-            await event.edit(str(CMD_HELP[args]))
+            await rambot.edit(str(CMD_HELP[args]))
         else:
-            await event.edit("**`Command Tidak Ditemukan, Harap Ketik Command Dengan Benar`**")
-            await asyncio.sleep(200)
-            await event.delete()
+            await rambot.edit("**`NGETIK YANG BENER NGENTOT!`**")
+            await asyncio.sleep(50)
+            await rambot.delete()
     else:
         string = ""
         for i in CMD_HELP:
             string += "`" + str(i)
-            string += "`\t 🖨  "
-        await event.edit("**PandaX_Userbot**\n\n"
-                         f"**◉ Bᴏᴛ ᴏꜰ PandaX_Userbot Plugin**\n**◉ Mᴏᴅᴜʟᴇꜱ : {len(CMD_HELP)}**\n\n"
-                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
-                         f"◉ {string}◉\n\n")
-        await event.reply(f"\n**Contoh** : Ketik <`.help afk`> Untuk Informasi Pengunaan.\nAtau Bisa Juga Ketik `.helpme` Untuk Main Menu Yang Lain-Nya.")
-        await asyncio.sleep(1000)
-        await event.delete()
+            string += f"`\t {EMOJI_HELP}  "
+        await rambot.edit(f"**{REPO_NAME}**\n\n"
+                         f"**{EMOJI_HELP} 𝙿𝙴𝙼𝙸𝙻𝙸𝙺 𝙱𝙾𝚃 : {DEFAULTUSER}**\n**{EMOJI_HELP}  𝙼𝙾𝙳𝚄𝙻𝙴𝚂 : {len(modules)}**\n\n"
+                         f"**{EMOJI_HELP} 𝚂𝙴𝙼𝚄𝙰 𝙼𝙴𝙽𝚄 :**\n\n ══════════╣❃ ♕ ❃╠══════════\n\n"
+                         f"{EMOJI_HELP} {string}\n\n ══════════╣❃ ♕ ❃╠══════════\n\nNGETIK YANG BENER YA NGENTOOOOT!!\n\n")
+        await rambot.reply(f"\n**Contoh** : Ketik <`.help ping`> Untuk Informasi Pengunaan.\nJangan Lupa Berdoa Sebelum Mencoba wahahaha..")
+        await asyncio.sleep(50)
+        await rambot.delete()
