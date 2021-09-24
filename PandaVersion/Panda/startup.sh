92eba790b22177dde769a56c0b83363b5e4e4fd2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-25 00:24:47 +0700

    Update startup.sh

--- PandaVersion/Panda/startup.sh
+++ PandaVersion/Panda/startup.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-# Ultroid - UserBot
-# Copyright (C) 2021 TeamUltroid
-#
-# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-
-
-echo "
-            PandaUserbot
-"
-
-python3 -m PandaX_Userbot
+https://github.com/ilhammansiz/PandaX_Userbot/archive/PandaUserbot.zip
