8d390f2e299c2a4e3ee13512da2ce6002988210c
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-25 10:41:53 +0700

    Update Toxic.py

--- PandaToxic/Toxic/Toxic.py
+++ PandaToxic/Toxic/Toxic.py
@@ -1,6 +1,6 @@
 from platform import uname
-from userbot import ALIVE_NAME, CMD_HELP
-from userbot.events import register
+from PandaToxic import ALIVE_NAME, CMD_HELP
+from PandaToxic.events import register
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
