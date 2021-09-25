54adaa7333e4b11e7140a4c70a23ba570980a1f1
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-25 21:40:16 +0700

    Create CommandToxic.py

---
+++ PandaToxic/Toxic/CommandToxic.py
@@ -0,0 +1,79 @@
+"""
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: P
+Usage: Untuk Memberi salam.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: L
+Usage: Untuk Menjawab Salam.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: K
+Usage: Untuk mengontoli mereka.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: N
+Usage: Kalo kesel coba aja.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: B
+Usage: Buat Ngatain Yang Suka Bacot.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: M
+Usage: Tersedak meledek.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: Y
+Usage: Buat yang males adu bacot.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: C
+Usage: Buat menghujat.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: S
+Usage: Haha sokap.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: V
+Usage: Hujat Orang caper.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: J
+Usage: Hujat Jamet.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: A
+Usage: Hujat yang gapunya muka.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: X
+Usage: Ngatain Grup wkwk.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: Z
+Usage: teruntuk petarung.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: H
+Usage: Coba dewek ah.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .atg
+Usage: Istighfar 1.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .ast
+Usage: Istighfar 2.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: O
+Usage: Ngatain org norak.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: G
+Usage: Liat Sendiri.
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .p
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .l
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gjm
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gjn
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gjb
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .yb
+Usage: Liat Sendiri
+
+𝘾𝙤𝙢𝙢𝙖𝙣𝙙: .gjk
+Usage: Liat Sendiri
+"""
