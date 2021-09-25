b9f34b90dfe973dddfa578c6646d9eba07e911b0
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-25 12:53:27 +0700

    Update and rename DaftarModules.py to DaftarToxicPanda.py

---
+++ PandaX_v20/DaftarToxicPanda.py
@@ -0,0 +1,67 @@
+"""
+P
+Usage: Untuk Memberi salam.
+
+L
+Usage: Untuk Menjawab Salam.
+
+K
+Usage: Untuk mengontoli mereka.
+
+N
+Usage: Kalo kesel coba aja.
+
+B
+Usage: Buat Ngatain Yang Suka Bacot.
+
+M
+Usage: Tersedak meledek.
+
+Y
+Usage: Buat yang males adu bacot.
+
+C
+Usage: Buat menghujat.
+
+S
+Usage: Haha sokap.
+V
+Usage: Hujat Orang caper.
+
+J
+Usage: Hujat Jamet.
+
+A
+Usage: Hujat yang gapunya muka.
+
+X
+Usage: Ngatain Grup wkwk.
+
+Z
+Usage: teruntuk petarung.
+
+H
+Usage: Coba dewek ah.
+
+.atg
+Usage: Istighfar 1.
+
+.ast
+Usage: Istighfar 2.
+
+O
+Usage: Ngatain org norak.
+
+G
+Usage: Liat Sendiri.
+
+.p
+.l
+.gjm
+.gjn
+.gjb
+.yb
+.gjk
+
+
+"""
