ba1d798853658eab8dd37e9240df40fd50410375
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-25 11:18:45 +0700

    Update utils.py

--- PandaX_Userbot/utils.py
+++ PandaX_Userbot/utils.py
@@ -94,8 +94,8 @@ def load_panda(plugin_name):
     if plugin_name.startswith("__"):
         pass
     elif plugin_name.endswith("_"):
-        path = Path(f"toxic/{plugin_name}.py")
-        name = "toxic.{}".format(plugin_name)
+        path = Path(f"PandaToxic/Toxic/{plugin_name}.py")
+        name = "PandaToxic/Toxic.{}".format(plugin_name)
         spec = util.spec_from_file_location(name, path)
         mod = util.module_from_spec(spec)
         spec.loader.exec_module(mod)
@@ -117,8 +117,8 @@ def load_panda(plugin_name):
         from .PandaVX._wrappers import eod, eor
         from PandaX_Userbot import CUSTOM_CMD
 
-        path = Path(f"toxic/{plugin_name}.py")
-        name = "toxic.{}".format(plugin_name)
+        path = Path(f"PandaToxic/Toxic/{plugin_name}.py")
+        name = "PandaToxic/Toxic.{}".format(plugin_name)
         spec = util.spec_from_file_location(name, path)
         mod = util.module_from_spec(spec)
         mod.asst = asst
@@ -179,7 +179,7 @@ def load_panda(plugin_name):
         modules["fridaybot.Config"] = xxx
         modules["userbot.uniborgConfig"] = xxx
         spec.loader.exec_module(mod)
-        modules["toxic." + plugin_name] = mod
+        modules["PandaToxic/Toxic." + plugin_name] = mod
         if not plugin_name.startswith("_"):
             try:
                 PANDA.append(plugin_name)
@@ -189,7 +189,7 @@ def load_panda(plugin_name):
                 else:
                     pass
             try:
-                doc = modules[f"toxic.{plugin_name}"].__doc__
+                doc = modules[f"PandaToxic/Toxic.{plugin_name}"].__doc__
                 HELP.update({f"{plugin_name}": doc.format(i=HNDLR)})
             except KeyError:
                 pass
