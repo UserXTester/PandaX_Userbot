cd9a39a2b1391f786706bdfb1348f15c6d4ccab0
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-27 00:04:38 +0700

    Update __main__.py

--- PandaX_Userbot/__main__.py
+++ PandaX_Userbot/__main__.py
@@ -117,6 +117,17 @@ if Hosted_On == "railway" and not udB.get("VCBOT"):
 
 plugin_loader(modules=modules, pmbot=pmbot, manager=manager, toxic=toxic)
 
+try:
+    os.system(
+        "git clone https://github.com/ilhammansiz/UserbotPanda xpanda/"
+    )
+except BaseException:
+    pass
+LOGS.info("Installing packages for modules")
+os.system("pip install -r xpanda/pandax.txt")
+
+plugin_loader(vcbot=vcbot)
+
 
 try:
     os.system(
