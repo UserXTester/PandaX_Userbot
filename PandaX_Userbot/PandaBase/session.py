006632e769887effd7b64710db5121859e5b2adb
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-27 00:40:04 +0700

    Update session.py

--- PandaX_Userbot/PandaBase/session.py
+++ PandaX_Userbot/PandaBase/session.py
@@ -1,12 +1,12 @@
 from pyrogram import Client as Bot
 from PandaX_Userbot import udB
-
+from PandaX_Userbot.Panda.database import Var
 
 
 bot = Bot(
     ":memory:",
-    API_ID,
-    API_HASH,
+    Var.API_ID,
+    Var.API_HASH,
     bot_token=udB.get("BOT_TOKEN"),
     plugins=dict(root="MusicBot.modules"),
 )
