8a68c7757b348ca67e089ef9d3961cc9048203fd
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-26 21:06:25 +0700

    Update __init__.py

--- MusicBot/services/converter/__init__.py
+++ MusicBot/services/converter/__init__.py
@@ -3,6 +3,6 @@ from os import listdir, mkdir
 if "raw_files" not in listdir():
     mkdir("raw_files")
 
-from GeezProject.services.converter.converter import convert
+from MusicBot.services.converter.converter import convert
 
 __all__ = ["convert"]
