ce2d929a8237a4754392088fe6117d749190a0d9
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-26 21:29:25 +0700

    Update callsmusic.py

--- MusicBot/services/callsmusic/callsmusic.py
+++ MusicBot/services/callsmusic/callsmusic.py
@@ -17,12 +17,11 @@
 
 from pyrogram import Client
 from pytgcalls import PyTgCalls
-
+from PandaX_Userbot import client
 import config
 from MusicBot.config import API_HASH, API_ID, SESSION_NAME
 from MusicBot.services.queues import queues
 
-client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 pytgcalls = PyTgCalls(client)
 
 
