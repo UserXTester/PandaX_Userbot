5cdd7f886ff46a1164624ce34c7270039582ef39
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 14:30:29 +0700

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -5,11 +5,23 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from tg_bot import DB_URI
 
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    database_url = (
+        DB_URI.replace("postgres:", "postgresql:")
+        if "postgres://" in DB_URI
+        else DB_URI
+    )
+    engine = create_engine(database_url)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-BASE = declarative_base()
-SESSION = start()
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    # this is a dirty way for the work-around required for #23
+    LOGS.error(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
+    LOGS.error(str(e))
