a3974d96154ad55ccbdfcfe02d06de56baf286eb
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 11:17:39 +0700

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -4,15 +4,12 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from tg_bot import DB_URI
 
-BASE = declarative_base()
-
 def start() -> scoped_session:
-    engine = create_engine(DB_URI)
+    engine = create_engine(DB_URI, client_encoding="utf8")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 
-
-
+BASE = declarative_base()
 SESSION = start()
