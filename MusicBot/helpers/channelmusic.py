983d1197dc2743dde8c6f1a14471847f1d04fb9c
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 22:51:18 +0700

    Update channelmusic.py

--- MusicBot/helpers/channelmusic.py
+++ MusicBot/helpers/channelmusic.py
@@ -1,7 +1,7 @@
-from pyrogram.types import Chat
+from telethon.tl.custom import Message
 
 
-def get_chat_id(chat: Chat):
+def get_chat_id(chat: Message):
     if chat.title.startswith("Channel Music: ") and chat.title[16:].isnumeric():
         return int(chat.title[15:])
     return chat.id
