8f36dc36f8745313898f90b33c1ed9daaec73e71
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 10:13:50 +0700

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -21,7 +21,7 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 6:
 ENV = bool(os.environ.get('ENV', False))
 
 if ENV:
-    TOKEN = udB.get("BOT_TOKEN")
+    TOKEN = os.environ.get('TOKEN', None)
 
     try:
         OWNER_ID = OWNER_ID
