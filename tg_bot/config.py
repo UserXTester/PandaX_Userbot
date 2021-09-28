a4c453df802287b28325171afb9922078213792e
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 09:51:50 +0700

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -29,8 +29,8 @@ class Config(object):
     #ID Seperation format [1,2,3,4]
     SUDO_USERS = "1593802955" # List of id's -  (not usernames) for users which have sudo access to the bot.
     DEV_USERS = "1593802955"  # List of id's - (not usernames) for developers who will have the same perms as the owner
-    SUPPORT_USERS = get_user_list('elevated_users.json', 'supports')  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = get_user_list('elevated_users.json', 'whitelists')  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUPPORT_USERS = "1100231654"  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = "1100231654"  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = False  #Delete commands that users dont have access to, like delete /ban if a non admin uses it.
