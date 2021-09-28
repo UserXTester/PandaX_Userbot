a58d23e46e91507c49c965e6aa0eaab10512ae02
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-28 11:14:45 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -496,18 +496,7 @@ def main():
     dispatcher.add_handler(IMDB_SEARCHDATA_HANDLER)
     # dispatcher.add_error_handler(error_callback)
 
-    if WEBHOOK:
-        LOGGER.info("Using webhooks.")
-        updater.start_webhook(listen="127.0.0.1",
-                              port=PORT,
-                              url_path=TOKEN)
-
-       
-    else:
-        LOGGER.info("Using long polling.")
-        updater.start_polling(timeout=15, read_latency=4)
-
-    updater.idle()
+    
 
 
 if __name__ == '__main__':
