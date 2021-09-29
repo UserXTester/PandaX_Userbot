3b3c1d6e8eac150c83628d0ca1242c7ccb8facd2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-09-29 09:56:34 +0700

    Update __init__.py

--- PandaX_ASsistant/__init__.py
+++ PandaX_ASsistant/__init__.py
@@ -4,6 +4,9 @@ from PandaX_Userbot import *
 from PandaX_Userbot.Panda.database import Var
 from PandaX_Userbot.functions.all import *
 from PandaXBahasa import get_languages, get_string
+from PandaX_Userbot.functions.asst_fns import *
+from PandaX_Userbot.functions.botchat_db import *
+
 
 OWNER_NAME = petercordpanda_bot.me.first_name
 OWNER_ID = petercordpanda_bot.me.id
