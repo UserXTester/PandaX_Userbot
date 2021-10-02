3b3cce4beb588533abb80bc8bd0ebfe210571338
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-10-02 23:18:01 +0700

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>
 import os
 from os import getenv
 from dotenv import load_dotenv
-from PandaX_Iserbot import udB
+from PandaX_Userbot import udB
 
 load_dotenv()
 
